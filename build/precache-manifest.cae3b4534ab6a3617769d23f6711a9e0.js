2961e92f22b3b2c22cc807d0b2d2e7a417ca25ce
Mahmoud Farghali <101347618@georgebrown.ca>
2022-10-09 22:59:17 -0400

    React project code added

---
+++ build/precache-manifest.cae3b4534ab6a3617769d23f6711a9e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44e53e47b610517fb5fa",
+    "url": "/Maoe1/comp3104/static/css/main.33a2716a.chunk.css"
+  },
+  {
+    "revision": "44e53e47b610517fb5fa",
+    "url": "/Maoe1/comp3104/static/js/main.44e53e47.chunk.js"
+  },
+  {
+    "revision": "65705389bd5cd5717083",
+    "url": "/Maoe1/comp3104/static/js/runtime~main.65705389.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/Maoe1/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "aadd3a5273318f3a0f9d17243ba0ace9",
+    "url": "/Maoe1/comp3104/index.html"
+  }
+];
